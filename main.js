45997ac3298935c6a6a277527255b484e5101488
Eduard <kozeychukk1997@gmail.com>
2023-10-08 14:55:05 +0300

    a function that calculates the sum of digits

---
+++ main.js
@@ -0,0 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    const NumberUserText = document.querySelector(".number_text")
+    const SendButton = document.querySelector(".send")
+    const SumNumberUserSendLine = document.querySelector(".sum_number_line")
+    
+    SendButton.addEventListener("click", function () {
+        const userEnteredtext = NumberUserText.value;
+    
+        function CalculateSumDigits(text) {
+            let sum = 0;
+            for (let i = 0; i < text.length; i++) {
+                const digit = parseInt(text.charAt(i));
+                if (!isNaN(digit)) {
+                    sum += digit;
+                }
+            }
+            return sum;
+    
+        }
+        const sumofDigits = CalculateSumDigits(userEnteredtext);
+        const SumGreaterThanLength = sumofDigits > userEnteredtext.length;
+            // Розрахунок суми цифр і порівняння з довжиною тексту
+        if (sumofDigits > userEnteredtext.length) {
+            SumNumberUserSendLine.textContent = `Сума цифр більша за довжину тексту: ${sumofDigits} > ${userEnteredtext.length}`;
+        } else {
+            SumNumberUserSendLine.textContent = `Сума цифр не більша за довжину тексту: ${sumofDigits} <= ${userEnteredtext.length}`;
+        }
+        console.log (sumofDigits)
+        console.log(SumGreaterThanLength);
+        
+        NumberUserText.value = "";
+    });   
+})
+
+
