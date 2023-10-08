93a29bbf1c087b00098f13a4532a8ae6c70af093
Eduard <kozeychukk1997@gmail.com>
2023-10-08 15:45:30 +0300

    explanatory comments plus a small function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+// document.addEventListener("DOMContentLoaded", function(){}это действие срабатывает когда весь HTML-документ был полностью загружен и обработан браузером
+// aункцbя что передается в addEventListener для действия DOMContentLoaded, 
+// выполняется после того как браузер загрузил HTML но до того как загрузились внешние ресурсы такие как катинки або стили 
 document.addEventListener("DOMContentLoaded", function(){
     const NumberUserText = document.querySelector(".number_text")
     const SendButton = document.querySelector(".send")
@@ -7,12 +9,13 @@ document.addEventListener("DOMContentLoaded", function(){
     SendButton.addEventListener("click", function () {
         const userEnteredtext = NumberUserText.value;
     
+        // функция что производит расчет суммы каждой цифры в веденной строке пользователем
         function CalculateSumDigits(text) {
             let sum = 0;
             for (let i = 0; i < text.length; i++) {
-                const digit = parseInt(text.charAt(i));
-                if (!isNaN(digit)) {
-                    sum += digit;
+                const digit = parseInt(text.charAt(i)); // parseInt
+                if (!isNaN(digit)) {    // isNaN - проводим проверку является ли digit числом если да то добавляем его к sum и если же 
+                    sum += digit;       // digit не является числом то просто его игнорируем.
                 }
             }
             return sum;
@@ -21,17 +24,37 @@ document.addEventListener("DOMContentLoaded", function(){
         const sumofDigits = CalculateSumDigits(userEnteredtext);
         console.log (sumofDigits)
         
+        //возвращаем true когда сума больше за длинну строки
         const SumGreaterThanLength = sumofDigits > userEnteredtext.length;
         console.log(SumGreaterThanLength);
 
-        if (sumofDigits > userEnteredtext.length) {
-            SumNumberUserSendLine.textContent = `Сума цифр більша за довжину тексту: ${sumofDigits} > ${userEnteredtext.length}`;
-        } else {
-            SumNumberUserSendLine.textContent = `Сума цифр не більша за довжину тексту: ${sumofDigits} <= ${userEnteredtext.length}`;
+        // функция что возвращает шнвертированое значение Boolean
+        function invertBoolean () {
+            return !SumGreaterThanLength;    
         }
+        console.log(invertBoolean())
+
+        // возвращаем true когда сума больше за длинну строки с текстом и возвращаем folse когда сумма числе меньше или равна длинне строки
+        // if (sumofDigits > userEnteredtext.length) {
+        //     SumNumberUserSendLine.textContent = `Сума цифр більша за довжину тексту: ${sumofDigits} > ${userEnteredtext.length}`;
+        // } else {
+        //     SumNumberUserSendLine.textContent = `Сума цифр не більша за довжину тексту: ${sumofDigits} <= ${userEnteredtext.length}`;
+        // }
         
-        NumberUserText.value = "";
+        // NumberUserText.value = "";
     });   
+
 })
 
 
+const str1 = "123456";
+const num1 = parseInt(str1); // num2 містить 4, число
+console.log (num1)
+
+const str2 = "4abcdef";
+const num2 = parseInt(str2); // num2 містить 4, число
+console.log (num2)
+
+const str3 = "ab4cdef";
+const num3 = parseInt(str3); // num3 містить NaN, не число
+console.log (num3)
